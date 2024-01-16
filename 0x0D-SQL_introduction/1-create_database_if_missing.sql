898de4dcee98b8ab1383fe93a433a8bc81526ab1
Eta-Argus1 <bourkali-ayoub@outlook.com>
2024-01-16 09:38:40 -0800

    message

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
---Creates the database hbtn_0c_0 in your MySQL server
---If the database hbtn_0c_0 already exists, your script should not fail
+-- Creates a database hbtn_0c_0 in your MySQL server
+-- if database already exits, script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
