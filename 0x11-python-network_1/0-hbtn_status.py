262b44ec32c88c7fd3f2e59b648493d49b067536
Eta-Argus1 <bourkali-ayoub@outlook.com>
2024-03-01 04:37:06 -0800

    message

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
-"""
+from urllib.request import Request, urlopen
 
-
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    req = Request(url)
+    with urlopen(req) as response:
+        page = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(page)))
+    print("\t- content: {}".format(page))
+    print("\t- utf8 content: {}".format(page.decode("utf-8")))
